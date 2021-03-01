4fb285c4fdc12687ffb97c8dc6c23bc728043a98
wontae <beegramin9@gmail.com>
2021-03-01 17:51:02 +0900

    빌트인 CreateAPIView

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from user.serializers import UserSerializer
 
-# Create your views here.
+# 빌트인 View
+# serializer를 이용해서 DB에 인스턴스를 createg하는 API를 만듦
+class CreateUserView(generics.CreateAPIView):
+    """ Create a new user in the system """
+    serializer_class = UserSerializer
