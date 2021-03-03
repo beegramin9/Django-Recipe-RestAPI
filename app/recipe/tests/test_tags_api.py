4f805fa99a8fdbcf4c3ac3b3cba44859c9725c9e
wontae <beegramin9@gmail.com>
2021-03-03 19:17:42 +0900

    Viewset에서 instance filter

---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,73 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from core.models import Tag
+from recipe.serializers import TagSerializer
+
+TAGS_URL = reverse('recipe:tag-list')
+
+
+class PublicTagsApiTests(TestCase):
+    """ Test the publicly available tags API """
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """ Test that login is required for retrieving tags """
+        # unauth된 request를 보냄
+        res = self.client.get(TAGS_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagsApiTests(TestCase):
+    """ Test the authorizeds user tags API """
+
+    def setUp(self):
+        # auth할때 쓸 user
+        self.user = get_user_model().objects.create_user(
+            'test@londonappdev.com',
+            'password123'
+        )
+        self.client = APIClient()
+        # 모든 request를 위에 만든 sample user로 authenticate
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """ Test retrieving tags """
+        # sample tags들을 먼저 몇개 만든 후
+        # API에 request를 보내고
+        # request 내의 tag와 내 tag가 같은지
+        Tag.objects.create(user= self.user, name='Vegan')
+        Tag.objects.create(user= self.user, name='Dessert')
+        res = self.client.get(TAGS_URL)
+        # - 붙여서 reversed alphabetical order
+        tags = Tag.objects.all().order_by('-name')
+
+        # tag를 serialize함( DB에서 받아왔으니 JSON으로)
+        serializer = TagSerializer(tags, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+    
+    # auth된 user의 tag만 들어오는지 테스트
+    def test_tags_limited_to_user(self):
+        """ Test that tags returned are for the authenticated user """
+        # setup에서 만든 user 말고 새 user(unathenticated) 만든 후
+        # 새 유저에 만들어논 tag를 assign
+        # 이 tag들은 request에 안 담겨져오길 예상
+        user2 = get_user_model().objects.create_user(
+            'other@londonappdev.com',
+            'testpass'
+        )
+        Tag.objects.create(user = user2, name = "Fruity")
+        tag = Tag.objects.create(user = self.user, name = "Comfort Food")
+
+        res = self.client.get(TAGS_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        # 1개 들어와야 함. 1개만 auth user니까
+        self.assertEqual(len(res.data), 1)
+        # response의 tag가 우리가 assign한게 맞는지
+        self.assertEqual(res.data[0]['name'], tag.name)
+ 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# Tag앱을 위한 ViewSet
 
-# Create your views here.
+# 요기 viewsets, mixins엔 이름처럼 다양한 viewset, mixin 있음
+# CUD 제외 R만 할거라서 GenericViewSet, ListModeMixin로 간단히 가능
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Tag
+from recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """ Manage tags in the database """
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    # list model할땐 모든 인스턴스틀 가져올 queryset 필요
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    """ AssertionError: 2 != 1 이 안나오려면
+    Viewset에서 unauth user를 filter해줘야하는데 안함
+    Viewset에 들어가면 queryset이 실행되고 밑에 filter customizing으로
+    filter된 애들이 API에서 보일 것 """
+    # overriding
+    def get_queryset(self):
+        """ Return objects for the current authenticated user only """
+        # request에 들어있는 user의 태그만
+        # 즉 test에서 force_authenticate 된 setUp의 user만 통과
+        return self.queryset.filter(user=self.request.user).order_by('-name')
